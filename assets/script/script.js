6fc90463d3db6d70644d1ac285e3168662fe7c7d
Alex Phillip <alexjoelphillip@gmail.com>
2022-12-29 01:32:27 +0000

    inital commit

---
+++ assets/script/script.js
@@ -0,0 +1,165 @@
+const container = document.querySelector('.container');
+const compactContainer = document.querySelector('compact-container');
+const header = document.querySelector('header');
+const main = document.querySelector('main');
+const orcaGrid = document.querySelector('.orca-grid');
+const compactBtn = document.querySelector('#compact');
+const darkTheme = document.querySelector('#dark-theme');
+const orangeBtn = document.querySelector('#orange');
+const amberWhiteStreakyBtn = document.querySelector('#amber-white-streaky');
+const redOpalBtn = document.querySelector('#red-opal');
+const sunflowerYellowBtn = document.querySelector('#sunflower-yellow');
+const blueStreakyBtn = document.querySelector('#blue-streaky');
+const frenchVanillaBtn = document.querySelector('#french-vanilla');
+const violetBtn = document.querySelector('#violet');
+const blackClearStreakyBtn = document.querySelector('#black-clear-streaky');
+const turquoiseWhiteStreakyBtn = document.querySelector(
+  '#turquoise-white-streaky'
+);
+const egyptianBlueBtn = document.querySelector('#egyptian-blue');
+const yellowRedStreakyBtn = document.querySelector('#yellow-red-streaky');
+const lightGreenBtn = document.querySelector('#light-green');
+const circles = document.querySelectorAll('.circle');
+
+compactBtn.addEventListener('click', () => {
+  container.classList.toggle('compact-container');
+  main.classList.toggle('compact-main');
+  header.classList.toggle('compact-header');
+  orcaGrid.classList.toggle('compact-orca-grid');
+});
+
+darkTheme.addEventListener('click', () => {
+  container.classList.toggle('dark-container');
+});
+
+orangeBtn.addEventListener('click', () => {
+  allOrange();
+});
+
+amberWhiteStreakyBtn.addEventListener('click', () => {
+  allAmberWhiteStreaky();
+});
+
+redOpalBtn.addEventListener('click', () => {
+  allRedOpal();
+});
+
+sunflowerYellowBtn.addEventListener('click', () => {
+  allSunflowerYellow();
+});
+
+blueStreakyBtn.addEventListener('click', () => {
+  allBlueStreaky();
+});
+
+frenchVanillaBtn.addEventListener('click', () => {
+  allFrenchVanilla();
+});
+
+violetBtn.addEventListener('click', () => {
+  allViolet();
+});
+
+blackClearStreakyBtn.addEventListener('click', () => {
+  allBlackClearStreaky();
+});
+
+turquoiseWhiteStreakyBtn.addEventListener('click', () => {
+  allTurquoiseWhiteStreaky();
+});
+
+egyptianBlueBtn.addEventListener('click', () => {
+  allEgyptianBlue();
+});
+
+yellowRedStreakyBtn.addEventListener('click', () => {
+  allYellowRedStreaky();
+});
+
+lightGreenBtn.addEventListener('click', () => {
+  allLightGreen();
+});
+
+const allOrange = () => {
+  circles.forEach((circle) => {
+    circle.src = './assets/img/orange.jpg';
+  });
+  orcaGrid.classList.toggle('solid-grid');
+};
+
+const allAmberWhiteStreaky = () => {
+  circles.forEach((circle) => {
+    circle.src = './assets/img/amber-white-streaky.jpg';
+  });
+  orcaGrid.classList.toggle('solid-grid');
+};
+
+const allRedOpal = () => {
+  circles.forEach((circle) => {
+    circle.src = './assets/img/red-opal.jpg';
+  });
+  orcaGrid.classList.toggle('solid-grid');
+};
+
+const allSunflowerYellow = () => {
+  circles.forEach((circle) => {
+    circle.src = './assets/img/sunflower-yellow.jpg';
+  });
+  orcaGrid.classList.toggle('solid-grid');
+};
+
+const allBlueStreaky = () => {
+  circles.forEach((circle) => {
+    circle.src = './assets/img/blue-streaky.jpg';
+  });
+  orcaGrid.classList.toggle('solid-grid');
+};
+
+const allFrenchVanilla = () => {
+  circles.forEach((circle) => {
+    circle.src = './assets/img/french-vanilla.jpg';
+  });
+  orcaGrid.classList.toggle('solid-grid');
+};
+
+const allViolet = () => {
+  circles.forEach((circle) => {
+    circle.src = './assets/img/violet.jpg';
+  });
+  orcaGrid.classList.toggle('solid-grid');
+};
+
+const allBlackClearStreaky = () => {
+  circles.forEach((circle) => {
+    circle.src = './assets/img/black-clear-streaky.jpg';
+  });
+  orcaGrid.classList.toggle('solid-grid');
+};
+
+const allTurquoiseWhiteStreaky = () => {
+  circles.forEach((circle) => {
+    circle.src = './assets/img/turquoise-white-streaky.jpg';
+  });
+  orcaGrid.classList.toggle('solid-grid');
+};
+
+const allEgyptianBlue = () => {
+  circles.forEach((circle) => {
+    circle.src = './assets/img/egyptian-blue.jpg';
+  });
+  orcaGrid.classList.toggle('solid-grid');
+};
+
+const allYellowRedStreaky = () => {
+  circles.forEach((circle) => {
+    circle.src = './assets/img/yellow-red-streaky.jpg';
+  });
+  orcaGrid.classList.toggle('solid-grid');
+};
+
+const allLightGreen = () => {
+  circles.forEach((circle) => {
+    circle.src = './assets/img/light-green.jpg';
+  });
+  orcaGrid.classList.toggle('solid-grid');
+};
